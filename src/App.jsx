bbba6cad802133f185f63087d317e4257029ba1b
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-07-30 13:05:24 +0100

    add the new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Navbar from './component/Navbar'
 import FilterProperty from './component/FilterProperty'
+import HouseCard from './component/HouseCard'
+import houses from './data/houses.json'
 function App() {
   return (
     <div className='app'>
@@ -12,6 +14,28 @@ function App() {
           <button className='btn btnExplore'>Explore Now</button>
         </article>
         <FilterProperty />
+      </section>
+      <section className=' container house'>
+        <nav className='houseNav'>
+          <h1>HOUSE TYPES</h1>
+          <input type="text" placeholder='Search here ....'/>
+        </nav>
+        <div className="houseCards">
+          {
+            houses.map((item,index)=>(
+              <HouseCard 
+                title= {item.title} 
+                img = {item.image} 
+                alt={item.alt} 
+                price = {item.price}
+              />
+            ))
+
+          }
+        </div>
+        <span className='moreHouse'>
+          <a href="#">More {">"}</a>
+        </span>
       </section>
     </div>
   )
